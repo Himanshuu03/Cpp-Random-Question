218824eb871c1cc94f42ddffbb54f24434f1f968
Himanshu <100996941+Himanshuu03@users.noreply.github.com>
2023-01-21 12:22:46 +0530

    Find Duplicate

---
+++ Find_Duplicate.cpp
@@ -0,0 +1,19 @@
+int duplicateNumber(int *arr, int size)
+{
+    for (int i = 0; i < size; i++) {
+      int count = 0;
+      for (int j = 0; j < size; j++) {
+        if (arr[i] == arr[j]) {
+          count++;
+        }
+      }
+      if (count > 1) {
+        return arr[i];
+      }
+    }
+    if (size == 1) {
+      return arr[0];
+    }
+    return -1;
+ 
+}
